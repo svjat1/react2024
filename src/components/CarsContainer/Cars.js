ff47a6d693e32859d6737e0e3b10a64a9fa8e23f
svjat1 <calipsossss@gmail.com>
2024-02-24 00:57:02 +0200

    lesson-6 just code3

--- src/components/CarsContainer/Cars.js
+++ src/components/CarsContainer/Cars.js
@@ -2,19 +2,38 @@ import {useEffect, useState} from "react";
 import {carService} from "../../services";
 import {Car} from "./Car";
 import {useAppContext} from "../../hooks/useAppContext";
+import {useSearchParams} from "react-router-dom";
 
 const Cars = () => {
     const [cars, setCars] = useState([])
-  const {trigger} = useAppContext()
+    const [query, setQuery] = useSearchParams({page: '1'})
+    const [prevNext, setPrevNext] = useState({prev: null, next: null})
+    const {trigger} = useAppContext()
 
-    useEffect(()=>{
-        carService.getAll().then(({data})=>{
+    useEffect(() => {
+        carService.getAll(query.get('page')).then(({data}) => {
             setCars(data.items)
+            setPrevNext({prev: data.prev, next: data.next})
         })
-    }, [trigger])
+    }, [trigger, query.get('page'), query])
+
+    const prev = () => {
+        setQuery(prev => {
+            prev.set('page', `${+prev.get('page') - 1}`)
+            return prev
+        })
+    }
+    const next = () => {
+        setQuery(prev => {
+            prev.set('page', `${+prev.get('page') + 1}`)
+            return prev
+        })
+    }
     return (
         <div>
-            {cars.map(car => <Car key={car.id}  car={car}/>)}
+            {cars.map(car => <Car key={car.id} car={car}/>)}
+            <button disabled={!prevNext.prev} onClick={prev}>Prev PAge</button>
+            <button disabled={!prevNext.next} onClick={next}>Next PAge</button>
         </div>
     );
 };
